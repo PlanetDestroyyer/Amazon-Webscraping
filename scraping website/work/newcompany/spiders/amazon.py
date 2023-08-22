c66af3e3c6957f4c95b4b5368d4064f518c03b6a
Pranav <59405252+PlanetDestroyyer@users.noreply.github.com>
2023-08-22 15:45:51 +0530

    part 1 done

--- scraping website/work/newcompany/spiders/amazon.py
+++ scraping website/work/newcompany/spiders/amazon.py
@@ -1,34 +1,46 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 class AmazonSpider(scrapy.Spider):
     name = "amazon"
     allowed_domains = ["www.amazon.in"]
     start_urls = [
         f"https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_{page}"
-        for page in range(1, 2)
+        for page in range(1, 21)  
     ]
     base_url = "https://www.amazon.in"
 
     def parse(self, response):
         products = response.css("div.s-result-list.s-search-results.sg-row div.s-asin")
         
-        count = 0
-        
         for product in products:
+            link = self.base_url + product.css("h2 a.a-link-normal::attr(href)").get()
             name = product.css("span.a-size-medium.a-color-base.a-text-normal::text").get()
             price = product.css("span.a-price-whole::text").get()
             rating = product.css("span.a-icon-alt::text").get()
             reviews = product.css("span.a-size-base.s-underline-text::text").get()  
-            product_link = product.xpath('h2/a/@href').get()
-
-
-                     
+            
             if name and price and rating and reviews:
                 yield {
-                    "Product Link" : product_link,
+                    "Product_link": link,
                     "Name": name.strip(),
-                    "Price": price + "rs",
+                    "Price": price + " rs",
                     "Rating": rating,
-                    "Reviews": reviews,  
+                    "Reviews": reviews,
                 }
-                count += 1 
+                
+                # After processing each product, follow the product link for Part 2 data
+                # yield scrapy.Request(link, callback=self.parse_product_details)
+
+    # def parse_product_details(self, response):
+    #     description = response.css("#productDescription p::text").get()
+    #     asin = response.css("th:contains('ASIN') + td::text").get()
+    #     product_description = response.css("div#productDescription p::text").get()
+    #     manufacturer = response.css("th:contains('Manufacturer') + td span::text").get()
+
+    #     yield {
+    #         "Description": description,
+    #         "ASIN": asin,
+    #         "Product_Description": product_description,
+    #         "Manufacturer": manufacturer
+    #     }
